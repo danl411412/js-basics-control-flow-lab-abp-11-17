767308eec7ea92b5251642afb23837b3bd1c1bb2
danl411412 <daan.b.lee@gmail.com>
2017-11-16 07:18:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num) {
+  if(num <= 400) {
+    return "This one is one me!";
+  } else if(num > 2000) {
+    return "I will gladly take your thirty bucks";
+  } else if(num > 2500) {
+    return "No can do.";
+  }
+}
