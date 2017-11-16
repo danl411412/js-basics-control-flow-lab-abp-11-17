22ad5e29aa072eb9f33dc2bce92c747b83f99e22
danl411412 <daan.b.lee@gmail.com>
2017-11-16 07:19:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(num) {
   if(num <= 400) {
     return "This one is on me!";
   } else if(num > 2000) {
-    return "I will gladly take your thirty bucks";
+    return "I will gladly take your thirty bucks.";
   } else if(num > 2500) {
     return "No can do.";
   }
