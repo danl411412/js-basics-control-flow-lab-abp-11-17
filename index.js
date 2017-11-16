e253a619fe1645f9398d0321a37be93bab92dbbb
danl411412 <daan.b.lee@gmail.com>
2017-11-16 07:26:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ function scuberGreetingForFeet(num) {
 function ternaryCheckCity(city) {
   return city === "NYC" ? "Ok, sounds good.":"No go."
 }
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
+}
